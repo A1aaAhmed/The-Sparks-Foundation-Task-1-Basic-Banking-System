f0682dfdb69c2d6d5021bf88f535cf34cbe045c3
A1aaAhmed <81260750+A1aaAhmed@users.noreply.github.com>
2022-02-21 04:55:11 +0200

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -23,13 +23,13 @@ if(isset($_POST['submit']))
     
 
 ?>
-<html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>transfer Money</title>
+  <title>Basic banking System</title>
   <link href="styles\style.css" rel="stylesheet">
 </head>
 <body>
