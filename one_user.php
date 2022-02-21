ca92df592af72199f17dfb62d1b682a5337aa9ee
A1aaAhmed <81260750+A1aaAhmed@users.noreply.github.com>
2022-02-21 04:55:45 +0200

    Update one_user.php

--- one_user.php
+++ one_user.php
@@ -5,15 +5,14 @@ $q="SELECT *FROM `myguests` WHERE `myguests`.`name`='{$name}'";
 $s=mysqli_query($con,$q);
 $row=mysqli_fetch_assoc($s);
 ?>
-<html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>transfer Money</title>
+  <title>Basic banking System</title>
   <link href="styles\style.css" rel="stylesheet">
-  
 </head>
 <body>
 <div ip="nav">
